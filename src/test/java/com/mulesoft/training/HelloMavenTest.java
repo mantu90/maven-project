d2dbd2b3f92442dff3cbff275a9364c21192efe4
juuso.mantila <juuso.mantila@gmail.com>
2018-10-23 11:39:44 +0300

    Added content-type header

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -7,9 +7,12 @@ import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
 
 public class HelloMavenTest extends FunctionalTestCase {
-    @Override
-    protected String getConfigFile() {
-        return "maven-project.xml";
+    
+    @Test
+    public void retrieveFlightsAddsAppropriateHeader() throws Exception {
+      MuleEvent event = runFlow("retrieveFlights");
+      String contentType = event.getMessage().getOutboundProperty("Content-Type");
+      assertEquals("application/json", contentType);
     }
     
     @Test
@@ -17,11 +20,9 @@ public class HelloMavenTest extends FunctionalTestCase {
         runFlowAndExpect("mavenFlow", "Hello Maven");
     }
     
-    @Test
-    public void retrieveFlightsAddsAppropriateHeader() throws Exception {
-      MuleEvent event = runFlow("retrieveFlights");
-      String contentType = event.getMessage().getOutboundProperty("Content-Type");
-      assertEquals("application/json", contentType);
+    @Override
+    protected String getConfigFile() {
+        return "maven-project.xml";
     }
 
 }
